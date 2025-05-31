3c67b5a21a4777e0ba8d7e566dfbcb9edb717e3a
Emma <chuixianghao@outlook.com>
2025-05-31 14:50:05 -0700

    basic jabascript

---
+++ script.js
@@ -0,0 +1,12 @@
+let name = "Emma";
+let age = 13;
+let LikeSushi = true;
+
+function greeting(name){
+        console.log("Hello",name);
+}
+
+
+    greeting("Emma");
+
+    document.getElementById("greeting");value
